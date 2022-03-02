6db49db7b9313b2d712addcaea23747f17328518
pepe lopez <plopez741@gmail.com>
2022-03-01 21:53:38 -0800

    commit tree a58f749e96ff3bdf618d2c0d16608c43435c248e

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter((string) => string.toUpperCase() === name.toUpperCase())
+}
+function fuzzyMatch(drivers, letters){
+    
+    return drivers.filter((string) => string.substring(0, 2) === letters)
+}
+function matchName(drivers, name){
+    return drivers.filter((string) => string.name === name)
+}
+findMatching(drivers, bobby)
